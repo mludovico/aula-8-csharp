57409e2e82f30af86e29340041c692d2068cd5ce
Marcelo Ludovico <maludovico@msn.com>
2021-01-24 14:32:39 -0300

    Adicionar arquivos de projeto.

---
+++ Genero/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Genero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contagemMulher = 0;
+            int contagemHomem = 0;
+            List<string> listaNomes = new List<string> { "Tadeu", "Alexandre", "Thaise", "Thamirys", "Matheus", "Mariana", "Valeska" };
+            foreach (string  nome in listaNomes)
+            {
+                bool opcaoValida = false;
+                while (!opcaoValida)
+                {
+                    Console.Write($"{nome} é homem ou mulher? H - Homem, M - Mulher: ");
+                    switch (Console.ReadLine().ToUpper())
+                    {
+                        case "H":
+                            contagemHomem++;
+                            opcaoValida = true;
+                            break;
+                        case "M":
+                            contagemMulher++;
+                            opcaoValida = true;
+                            break;
+                        default:
+                            Console.WriteLine("Opção inválida");
+                            opcaoValida = false;
+                            break;
+                    } 
+                }
+            }
+            Console.WriteLine($"{contagemMulher} são mulheres e {contagemHomem} são homens.");
+        }
+    }
+}
---
+++ Paridade/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Paridade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 20; i++)
+            {
+                string paridade = i % 2 == 0 ? "par" : "ímpar";
+                Console.WriteLine($"O número {i} é {paridade}");
+            }
+        }
+    }
+}
